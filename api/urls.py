3cdf09b01a40489a40aec40ca7c38a4448775eb1
Astra López <astra.lopez.zagal@gmail.com>
2024-06-11 09:48:24 +0000

    Se forma la base del CRUD

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.ApiOverview, name='home')
+  path('', views.ApiOverview, name='home'),
+  path('create/', views.add_comic, name='add-comic'),
+  path('all/', views.view_comics, name='view-comics'),
+  path('update/<int:pk>/', views.update_comics, name='update-comics'),
+  path('item/<int:pk>/delete/', views.delete_comics, name='delete-comics'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import serializers, status
 from .models import Comic
 from .serializer import ComicSerializer
 
@@ -18,3 +19,56 @@ def ApiOverview(request):
   }
 
   return Response(api_urls)
+
+# Create view
+@api_view(['POST'])
+def add_comic(request):
+  comic = ComicSerializer(data=request.data)
+
+  # Validar que no exista aún
+  if Comic.objects.filter(**request.data).exists():
+    raise serializers.ValidationError('This data already exists')
+
+  if comic.is_valid():
+    comic.save()
+    return Response(comic.data)
+  else:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+# Read view
+@api_view(['GET'])
+def view_comics(request):
+  # Buscar paremetros en la URL
+  if request.query_params:
+    comics = Comic.objects.filter(**request.query_params.dict())
+  else:
+    comics = Comic.objects.all()
+
+  # Si no hay nada en comics, tirar error
+  if comics:
+    serializer = ComicSerializer(comics, many=True)
+    return Response(serializer.data)
+  else:
+    return Response(status.HTTP_404_NOT_FOUND)
+  
+# Update view
+@api_view(['POST'])
+def update_comics(request, pk):
+  comic = Comic.objects.get(pk=pk)
+  data = ComicSerializer(instance=comic, data=request.data)
+
+  # Si los datos no son validos, tirar error
+  if data.is_valid():
+    data.save()
+    return Response(data.data)
+  else: 
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+# Delete view
+@api_view(['DELETE'])
+def delete_comics(request, pk):
+  comic = get_object_or_404(Comic, pk=pk)
+  comic.delete()
+  return Response(status=status.HTTP_202_ACCEPTED)
+
+# Wow, that's a whole CRUD right there! We are almost done! [citation needed]
